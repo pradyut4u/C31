65056aa4826746e663896828543bec50689000ef
pradyut4u <60035671+pradyut4u@users.noreply.github.com>
2020-03-19 20:23:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var snake;
+var rez = 20;
+var food;
+var w;
+var h;
+var score
+function setup() {
+  createCanvas(600, 600);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake = new Snake();
+  foodLocation();
+  score=0
+}
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (key == ' ') {
+    snake.grow();
+  }
+
+}
+
+function draw() {
+  scale(rez);
+  background(220);
+  if (snake.eat(food)) {
+      score=score+5
+    foodLocation();
+  }
+  console.log(score)
+  snake.update();
+  snake.show();
+
+
+  if (snake.endGame()) {
+    print("END GAME");
+    background(255, 0, 0);
+    noLoop();
+  }
+
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
+}
